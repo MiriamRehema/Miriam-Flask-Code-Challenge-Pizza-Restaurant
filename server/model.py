1ddff965c255e6af7f9d394adca1db497bd5d164
MiriamRehema <rehemamiriam30@gmail.com>
2023-09-27 10:33:35 +0300

    model.py

--- server/model.py
+++ server/model.py
@@ -1,35 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
-#from sqlalchemy import CheckConstraint
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData
 
+metadata=MetaData(naming_convention={
+    "fk":"fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
 
-db = SQLAlchemy()
-class Restaurant(db.Model):
+class Restaurant(db.Model,SerializerMixin):
     __tablename__='restaurant'
 
+    serialize_rules=('-pizzas.restaurant','-restaurant.pizza')
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    address=db.column(db.String,unique=True,nullable=False)
+    address=db.Column(db.String,unique=True, nullable=False)
 
-    @validates('name',)
-    def validate_name(self,unique,name):
-        if not name or len(name)<50:
-            raise ValueError("Name not found please entre another name")or("Name must be at least 50 characters long.")
+    pizzas=db.relationship('RestaurantPizza',backref='restaurants')
+
+    @validates(name)
+    def validate_name(self, key, value):
+        if  len(value) >50:
+            raise ValueError("Name must be at less than 50 characters long.")
             
-        return name
-    
-   
+        return value
+  
 
-        
-
-    def __repr__(self):
-        return f'Restaurant(id={self.id}, name={self.name},address={self.address})'
-    
-
-
-class Pizzas(db.Model):
+class Pizza(db.Model,SerializerMixin):
     __tablename__ = 'pizza'
+    serialize_rules=('-restaurant.pizza','-restaurant.pizza')
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
@@ -37,32 +36,26 @@ class Pizzas(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    restaurants=db.relationship('RestaurantPizza',backref='pizza')
 
 
-
-
-    def __repr__(self):
-        return f'Pizzas(id={self.id}, name={self.name} ,ingredient={self.ingredient})'
-
-
-class RestaurantPizza(db.Model):
+class RestaurantPizza(db.Model,SerializerMixin):
     __tablename__ = 'restaurantpizza'
-
-    restaurant_id = db.Column(db.Integer, primary_key=True)
-    pizza_id = db.Column(db.Integer, primary_key=True)
-    price= db.Column(db.Integer, primary_key=True)
+    serialize_rules=('-restaurant','pizza')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    price= db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
 
-    @validates('price',)
-    def validate_price(self,unique,price):
-        if not 1 <= price <= 30:
+    @validates(price)
+    def validate_price(self, key, value):
+        if not 1 <= value <= 30:
             raise ValueError("Price should be  found between 1 and 30")
             
-        return price
+        return value
 
-    def __repr__(self):
-        return f'RestaurantPizza(price={self.price}, restaurant_id={self.restaurant_id} pizza_id={self.pizza_id}, )'
-
-
+   
